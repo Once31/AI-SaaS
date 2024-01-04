33f4e56249d1ce16a9eeb30d72afc3a4f9315b47
once21 <ajay.chavan@pointmatrix.in>
2024-01-04 22:21:06 +0530

    feat/code and image ai model

--- app/(dashboard)/(routes)/code/page.tsx
+++ app/(dashboard)/(routes)/code/page.tsx
@@ -1,24 +1,25 @@
 "use client";
 
-import { zodResolver } from "@hookform/resolvers/zod";
-import axios from "axios";
+import { BotAvatar } from "@/components/bot-avatar";
+import Empty from "@/components/empty";
 import Heading from "@/components/heading";
-import { MessageSquare } from "lucide-react";
-import { useForm } from "react-hook-form";
-import { formInfer, formSchema } from "./constants";
+import { Loader } from "@/components/loader";
+import { Button } from "@/components/ui/button";
 import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
-import { ChatCompletionMessageParam } from "openai/resources/index.mjs";
-import Empty from "@/components/empty";
-import { Loader } from "@/components/loader";
-import { cn } from "@/lib/utils";
 import { UserAvatar } from "@/components/user-avatar";
-import { BotAvatar } from "@/components/bot-avatar";
+import { cn } from "@/lib/utils";
+import { formInfer, formSchema } from "@/validators";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
+import { Code } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { ChatCompletionMessageParam } from "openai/resources/index.mjs";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import ReactMarkdown from "react-markdown";
 
-const ConversationPage = () => {
+const CodeGenerationPage = () => {
   const router = useRouter();
   const [messages, setMessages] = useState<ChatCompletionMessageParam[]>([]);
 
@@ -29,8 +30,6 @@ const ConversationPage = () => {
     },
   });
 
-  const isLoading = form.formState.isSubmitting;
-
   const onSubmit = async (values: formInfer) => {
     try {
       const userMessage: ChatCompletionMessageParam = {
@@ -39,30 +38,29 @@ const ConversationPage = () => {
       };
 
       const newMessages = [...messages, userMessage];
-
-      const response = await axios.post("/api/conversation", {
+      const response = await axios.post("/api/code", {
         messages: newMessages,
       });
 
       setMessages((current) => [...current, userMessage, response.data]);
 
       form.reset();
-    } catch (error: any) {
-      // TODO Open Pro Modal
+    } catch (error) {
       console.log(error);
     } finally {
       router.refresh();
     }
   };
 
+  const isLoading = form.formState.isSubmitting;
   return (
     <div>
       <Heading
-        title="Conversation"
-        description="Our most advanced conversation model."
-        icon={MessageSquare}
-        iconColor="text-violet-500"
-        bgColor="bg-violet-500/10"
+        title="Code Generation"
+        description="Generate code using descriptive text"
+        icon={Code}
+        iconColor="text-green-700"
+        bgColor="bg-green-700/10"
       />
       <div className="px-4 md:px-8">
         <div>
@@ -73,13 +71,14 @@ const ConversationPage = () => {
             >
               <FormField
                 name="prompt"
+                control={form.control}
                 render={({ field }) => (
                   <FormItem className="col-span-12 lg:col-span-10">
                     <FormControl className="m-0 p-0">
                       <Input
                         className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
                         disabled={isLoading}
-                        placeholder="How do I calculate the radius of a circle?"
+                        placeholder="Simple input with submit button using reactjs"
                         {...field}
                       />
                     </FormControl>
@@ -111,9 +110,21 @@ const ConversationPage = () => {
                 )}
               >
                 {message.role === "user" ? <UserAvatar /> : <BotAvatar />}
-                <p className="text-sm self-center">
-                  {message.content as string}
-                </p>
+                <ReactMarkdown
+                  components={{
+                    pre: ({ node, ...props }) => (
+                      <div className="overflow-y-auto w-full my-2 bg-black/10 p-2 rounded-lg">
+                        <pre {...props} />
+                      </div>
+                    ),
+                    code: ({ node, ...props }) => (
+                      <code className="bg-black/10 rounded-lg p-1" {...props} />
+                    ),
+                  }}
+                  className={"text-sm overflow-hidden leading-7"}
+                >
+                  {(message.content as string) || ""}
+                </ReactMarkdown>
               </div>
             ))}
           </div>
@@ -123,4 +134,4 @@ const ConversationPage = () => {
   );
 };
 
-export default ConversationPage;
+export default CodeGenerationPage;
