8b1a7fef82d3dee718cfaa559c219dfe129b5d49
once21 <ajay.chavan@pointmatrix.in>
2024-01-10 00:15:17 +0530

    feat: mobile view premium card

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,10 +1,14 @@
 import { UserButton } from "@clerk/nextjs";
 import MobileSidebar from "./mobile-sidebar";
 
-const Navbar = () => {
+interface NavbarProps {
+  apiLimitCount: number;
+}
+
+const Navbar = ({ apiLimitCount }: NavbarProps) => {
   return (
     <div className="flex items-center p-4">
-      <MobileSidebar />
+      <MobileSidebar apiLimitCount={apiLimitCount} />
       <div className="flex justify-end flex-1">
         <UserButton afterSignOutUrl="/" />
       </div>
